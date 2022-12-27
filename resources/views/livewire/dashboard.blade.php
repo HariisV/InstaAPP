5f941be129e333c9ee7c7a421dc4d0afa3efd652
Haris Wahyudi <hariswh9@gmail.com>
2022-12-27 23:26:44 +0700

    Setup Database Structur & Migration Database

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,5 +1,26 @@
-<div class="max-w-7xl mx-auto mt-10 sm:px-6 lg:px-8">
-    <div class="bg-white w-full h-full overflow-hidden p-5 rounded-xl">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt earum cupiditate tempora totam ab recusandae explicabo assumenda quod delectus. Suscipit rem neque cum repudiandae tempora culpa facilis dignissimos quisquam molestias?
+<div class="">
+    <div class="w-1/2 mx-auto mt-10 sm:px-6 lg:px-8">
+        <div class="bg-white w-full h-full overflow-hidden p-5 rounded-xl">
+            <div class="flex mb-4">
+                <img src="https://ui-avatars.com/api/?name=H+W&color=7F9CF5&background=EBF4FF" class="w-8 h-8 rounded-full" alt="">
+                <p class="ml-2 text-gray-700 self-center text-sm">
+                    {{ Auth()->user()->name  }}
+                </p>
+            </div>
+            <textarea name="" id="" cols="30" rows="3" class="w-full h-full text-xs border border-gray-300 active:border-gray-900"></textarea>
+            <input type="file" class="border border-gray-300 w-full" accept="image/*">
+            <div class="w-full text-right mt-3">
+                <button class=" bg-gray-300 text-xs py-2 px-5 rounded-full">
+                    Send
+                </button>
+           </div>
+        </div>
     </div>
+
+    <div class="w-1/2 mx-auto mt-10 sm:px-6 lg:px-8">
+        <div class="bg-white w-full h-full overflow-hidden p-5 rounded-xl">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt earum cupiditate tempora totam ab recusandae explicabo assumenda quod delectus. Suscipit rem neque cum repudiandae tempora culpa facilis dignissimos quisquam molestias?
+        </div>
+    </div>
+    
 </div>
